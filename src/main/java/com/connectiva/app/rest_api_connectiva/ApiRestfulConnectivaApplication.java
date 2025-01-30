843704dded4111881ec83a31047b3eb8f476ecb4
Marcos <marcos.vinicius.dev.2004@gmail.com>
2025-01-30 14:38:54 -0300

    1º Commit - Iniciando Versionamento do Projeto

---
+++ src/main/java/com/connectiva/app/rest_api_connectiva/ApiRestfulConnectivaApplication.java
@@ -0,0 +1,13 @@
+package com.connectiva.app.rest_api_connectiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulConnectivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulConnectivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/connectiva/app/rest_api_connectiva/ApiRestfulConnectivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectiva.app.rest_api_connectiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulConnectivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
