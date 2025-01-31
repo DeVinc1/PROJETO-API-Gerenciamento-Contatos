c073bb937dfa535ffe442e64479db6db7c249c33
Marcos <marcos.vinicius.dev.2004@gmail.com>
2025-01-31 15:16:29 -0300

    Adicionar pesquisa de contato por valor de ID

--- src/main/java/com/connectiva/app/rest_api_connectiva/service/ContactService.java
+++ src/main/java/com/connectiva/app/rest_api_connectiva/service/ContactService.java
@@ -4,7 +4,6 @@ import com.connectiva.app.rest_api_connectiva.model.Contact;
 import com.connectiva.app.rest_api_connectiva.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
