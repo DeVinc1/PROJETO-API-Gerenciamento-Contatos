c897019388c4579339d610e0eef7600b8afc6e79
Marcos <marcos.vinicius.dev.2004@gmail.com>
2025-02-01 22:31:00 -0300

    Retirar função obsoleta

--- src/main/java/com/connectiva/app/rest_api_connectiva/repository/ContactRepository.java
+++ src/main/java/com/connectiva/app/rest_api_connectiva/repository/ContactRepository.java
@@ -3,6 +3,8 @@ package com.connectiva.app.rest_api_connectiva.repository;
 import com.connectiva.app.rest_api_connectiva.model.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ContactRepository extends JpaRepository <Contact, Long> {
 
 }
