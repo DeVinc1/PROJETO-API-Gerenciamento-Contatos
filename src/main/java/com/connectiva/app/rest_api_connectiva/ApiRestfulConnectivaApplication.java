4fc6b3b43c9893823a8a95ac569b3f93800a98ca
Marcos <marcos.vinicius.dev.2004@gmail.com>
2025-02-03 14:43:37 -0300

    Criar mensagem padrão para a API

--- src/main/java/com/connectiva/app/rest_api_connectiva/ApiRestfulConnectivaApplication.java
+++ src/main/java/com/connectiva/app/rest_api_connectiva/ApiRestfulConnectivaApplication.java
@@ -2,12 +2,28 @@ package com.connectiva.app.rest_api_connectiva;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("api/v2")
 public class ApiRestfulConnectivaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestfulConnectivaApplication.class, args);
 	}
 
+	@GetMapping
+	public List<String> welcomeDefaultMassage(){
+		return List.of("Welcome to the Connectiva RESTful API",
+				"api: Connectiva API REST",
+				"status: Online and Running",
+				"version: 2.0",
+				"description: API RESTful to manage data in a contact database ",
+				"documentation: TBD");
+	}
 }
