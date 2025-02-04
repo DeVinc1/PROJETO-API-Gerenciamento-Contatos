6d856171ce81ff91e39623a5224c180ded23102c
Marcos <marcos.vinicius.dev.2004@gmail.com>
2025-02-04 13:48:52 -0300

    Atualizar projeto para evitar testes

--- src/test/java/com/connectiva/app/rest_api_connectiva/ApiRestfulConnectivaApplicationTests.java
+++ src/test/java/com/connectiva/app/rest_api_connectiva/ApiRestfulConnectivaApplicationTests.java
@@ -2,10 +2,13 @@ package com.connectiva.app.rest_api_connectiva;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.datasource.url=",
+		"spring.datasource.username=",
+		"spring.datasource.password=",
+		"spring.datasource.driver-class-name="
+})
 class ApiRestfulConnectivaApplicationTests {
 
 	@Test
